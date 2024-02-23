eef8e7fa79065876356c96a3749660016dcdd360
ToastexGD <161091070+ToastexGD@users.noreply.github.com>
2024-02-23 18:33:17 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Request Channels","Made by ToastexGD"];
 
 
 let currentIndex = 0;
